956c94eb7bc11df6b7af4a48817793eb006d2c1f
Ingmar Forsberg <ingmar_aa_f@hotmail.com>
2024-03-06 20:14:26 +0100

    created line swarm

--- src/main/java/Actors/Coordinates.java
+++ src/main/java/Actors/Coordinates.java
@@ -10,50 +10,16 @@ import java.util.Random;
 
 public class Coordinates extends Vector2 {
 
-//    public static final List<Vector2> spawnPoints = Arrays.asList(
-//
-//            // along left side of screen
-//            new Vector2((float)-0.5*Gdx.graphics.getWidth(),0),
-//            new Vector2((float)-0.5*Gdx.graphics.getWidth(), (float)0.5*Gdx.graphics.getHeight()),
-//            new Vector2((float)-0.5*Gdx.graphics.getWidth(), (float)Gdx.graphics.getHeight()),
-//
-//            // along top of screen
-//            new Vector2(0, (float)1.5*Gdx.graphics.getHeight()),
-//            new Vector2((float)0.5*Gdx.graphics.getWidth(), (float)1.5*Gdx.graphics.getHeight()),
-//            new Vector2((float)Gdx.graphics.getWidth(), (float)1.5*Gdx.graphics.getHeight()),
-//
-//
-//            // along right side of screen
-//            new Vector2((float)1.5*Gdx.graphics.getWidth(), (float)Gdx.graphics.getHeight()),
-//            new Vector2((float)1.5*Gdx.graphics.getWidth(), (float)0.5*Gdx.graphics.getHeight()),
-//            new Vector2((float)1.5*Gdx.graphics.getWidth(), 0),
-//
-//            //along bottom of screen
-//            new Vector2(0, (float)-0.5*Gdx.graphics.getHeight()),
-//            new Vector2((float)0.5*Gdx.graphics.getWidth(), (float)-0.5*Gdx.graphics.getHeight()),
-//            new Vector2((float)Gdx.graphics.getWidth(), (float)-0.5*Gdx.graphics.getHeight())
-//    );
-
-
     public Coordinates(float x, float y) {
-        super(x,y);
-    }
 
-    /**
-     *
-     * @return random start coordinate from the spawn points list
-     */
-    public static Vector2 random() {
-        Random random = new Random();
-        int randomIndex = random.nextInt(spawnPoints().size());
-        return spawnPoints().get(randomIndex);
+        super(x,y);
     }
 
     /**
      * Creating a list of coordinates for the swarm
      * @param numMembers
      * @param startPoint
-     * @return
+     * @return a list of coordinates that forms a square grid
      */
     public static List<Vector2> squareSwarm(int numMembers, Vector2 startPoint, int spacing) {
         float sideLength = (float)Math.ceil(Math.sqrt(numMembers));
@@ -64,36 +30,57 @@ public class Coordinates extends Vector2 {
                 swarmCoordinates.add(newCor);
             }
         }
+
         return swarmCoordinates;
     }
-    public static List<Vector2> lineSwarm(int numMembers, Vector2 startPoint, int spacing) {
+
+    /**
+     *
+     * @param numMembers
+     * @param centerPoint
+     * @param spacing
+     * @param target
+     * @return list coordinates that forms a line
+     */
+    public static List<Vector2> lineSwarm(int numMembers,Vector2 centerPoint, int spacing, Vector2 target) {
+        Vector2 targetCoordinate = new Vector2(800, 500);
         List<Vector2> swarmCoordinates = new ArrayList<>();
 
+        // Calculate the direction vector from centerPoint to targetCoordinate
+        Vector2 direction = new Vector2(targetCoordinate.x - centerPoint.x, targetCoordinate.y - centerPoint.y).nor();
+
+        // Calculate the perpendicular vector (normal) to the direction vector
+        Vector2 normal = new Vector2(-direction.y, direction.x);
+
+        // Adjust the starting position based on the direction and normal vectors
+        float startX = centerPoint.x - (numMembers - 1) / 2f * spacing * normal.x;
+        float startY = centerPoint.y - (numMembers - 1) / 2f * spacing * normal.y;
+
+        // Create the swarm coordinates based on the adjusted starting position
+        for (int i = 0; i < numMembers; i++) {
+            swarmCoordinates.add(new Vector2(startX + i * spacing * normal.x, startY + i * spacing * normal.y));
+        }
+
+        return swarmCoordinates;
     }
-    private static List<Vector2> spawnPoints() {
-        return Arrays.asList(
-
-                // along left side of screen
-                new Vector2((float)-0.5*Gdx.graphics.getWidth(),0),
-                new Vector2((float)-0.5*Gdx.graphics.getWidth(), (float)0.5*Gdx.graphics.getHeight()),
-                new Vector2((float)-0.5*Gdx.graphics.getWidth(), (float)Gdx.graphics.getHeight()),
-
-                // along top of screen
-                new Vector2(0, (float)1.5*Gdx.graphics.getHeight()),
-                new Vector2((float)0.5*Gdx.graphics.getWidth(), (float)1.5*Gdx.graphics.getHeight()),
-                new Vector2((float)Gdx.graphics.getWidth(), (float)1.5*Gdx.graphics.getHeight()),
 
 
-                // along right side of screen
-                new Vector2((float)1.5*Gdx.graphics.getWidth(), (float)Gdx.graphics.getHeight()),
-                new Vector2((float)1.5*Gdx.graphics.getWidth(), (float)0.5*Gdx.graphics.getHeight()),
-                new Vector2((float)1.5*Gdx.graphics.getWidth(), 0),
+    /**
+     *
+     * @return a random coordinates outside of screen
+     */
+    public static Vector2 randomPoint() {
 
-                //along bottom of screen
-                new Vector2(0, (float)-0.5*Gdx.graphics.getHeight()),
-                new Vector2((float)0.5*Gdx.graphics.getWidth(), (float)-0.5*Gdx.graphics.getHeight()),
-                new Vector2((float)Gdx.graphics.getWidth(), (float)-0.5*Gdx.graphics.getHeight())
-        );
+        Vector2 center = new Vector2((float)Gdx.graphics.getWidth()/2, (float)Gdx.graphics.getHeight()/2);
+        double innerRadius = (float)Gdx.graphics.getWidth()/2;
+        double outerRadius = Gdx.graphics.getWidth();
+        double angle = Math.toRadians(Math.random() * 360);
+        double randomRadius = innerRadius + Math.random() * (outerRadius - innerRadius);
+        double x = center.x + randomRadius * Math.cos(angle);
+        double y = center.y + randomRadius * Math.sin(angle);
+
+        return new Vector2((float)x,(float)y);
+
     }
 
 
