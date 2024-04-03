60257ee14faf32436b77331af33774bacdc68942
Ingmar Forsberg <ingmar_aa_f@hotmail.com>
2024-04-03 14:22:23 +0200

    terrain handling

---
+++ src/main/java/Terrain/TerrainFactory.java
@@ -0,0 +1,14 @@
+package Terrain;
+
+import java.util.List;
+
+public abstract class TerrainFactory {
+
+    public static Terrain create(String type) {
+        return null;
+    }
+
+    public static List<Terrain> create(String type, int num) {
+        return null;
+    }
+}
