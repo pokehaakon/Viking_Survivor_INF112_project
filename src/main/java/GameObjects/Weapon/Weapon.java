f5be85a2fb74c530840f894fe8491857cbc7258c
Ingmar Forsberg <ingmar_aa_f@hotmail.com>
2024-04-09 12:39:15 +0200

    untracked files, need to commit to checkout branch

---
+++ src/main/java/GameObjects/Weapon/Weapon.java
@@ -0,0 +1,10 @@
+package GameObjects.Weapon;
+
+import GameObjects.BodyFeatures;
+import GameObjects.GameObject;
+import com.badlogic.gdx.Game;
+
+public abstract class Weapon extends GameObject {
+
+    public Weapon(BodyFeatures bodyFeatures, float scale, )
+}
