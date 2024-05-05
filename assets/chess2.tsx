7ec9668343137b30b96bad521ceb97acac00a865
Eilif Eilifsen <e.eilifsen@gmail.com>
2024-05-05 09:19:17 +0200

    removed/relocated files

--- assets/chess2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="pixilart-drawing" tilewidth="1" tileheight="1" tilecount="1024" columns="32">
- <image source="pixilart-drawing.png" width="32" height="32"/>
-</tileset>
